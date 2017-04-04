8a3277451063e66eb82778808f9580eae35337ae
Bleep-Bloop <TaisenColcher@Gmail.com>
2017-04-04 01:22:03 -0400

    Worked on tank movement/controls
    Not finished (joystick not working properly)

---
+++ ArmouredAssault/Assets/TankController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		goForward ();
+
+	}
+
+
+	public void goForward() 
+	{
+
+		transform.Translate (Vector3.forward * (speed) * Time.deltaTime);
+
+	}
+
+
+}
