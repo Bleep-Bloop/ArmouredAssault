7c4a89d4caa771234015d7e1729182da45a65e89
Bleep-Bloop <TaisenColcher@Gmail.com>
2017-04-05 02:23:37 -0400

    Enemy AI find and attack setup

---
+++ ArmouredAssault/Assets/EnemySIght.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySIght : MonoBehaviour {
+
+
+	public EnemyAI thisEnemy;
+	public bool canFire;
+
+	// Use this for initialization
+	void Start () {
+
+		canFire = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		thisEnemy = GetComponentInParent<EnemyAI> ();
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.tag == "Player" && canFire == true) {
+
+			Debug.Log ("Fire at player");
+
+			canFire = false;
+
+			thisEnemy.fire ();
+
+			StartCoroutine (waiting ());
+
+
+		}
+
+	}
+
+
+	//Time untill next shot
+	private IEnumerator waiting()
+	{
+		yield return new WaitForSecondsRealtime (2.0f);
+		canFire = true;
+
+	}
+
+
+
+}
